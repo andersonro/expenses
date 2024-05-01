6de2384bae46a2cb6b2df8f566555f74fbc038b9
Anderson Roberto de Oliveira <andersonrobertobru@gmail.com>
2024-05-01 12:13:08 -0300

    FIT, listView com itens do elemento

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -5,6 +5,7 @@ import 'package:expenses/view/card_transaction_widget.dart';
 import 'package:expenses/view/chart_card_widget.dart';
 import 'package:expenses/view/transaction_form_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -87,10 +88,36 @@ class _HomePageState extends State<HomePage> {
                         itemCount: _transactions.length,
                         itemBuilder: (context, index) {
                           var transaction = _transactions[index];
-                          return CardTransactionWidget(
-                              value: transaction.value,
-                              date: transaction.date,
-                              title: transaction.title);
+                          return Card(
+                            elevation: 4,
+                            color: const Color.fromARGB(255, 248, 247, 247),
+                            shadowColor: Theme.of(context).primaryColor,
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Theme.of(context).primaryColor,
+                                radius: 30,
+                                child: Padding(
+                                  padding: EdgeInsets.all(6),
+                                  child: FittedBox(
+                                    child: Text(
+                                      'R\$ ${transaction.value.toStringAsFixed(2)}',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              title: Text(transaction.title),
+                              subtitle: Text(
+                                DateFormat('dd/MM/yyyy HH:mm:ss')
+                                    .format(transaction.date),
+                                style: const TextStyle(fontSize: 10),
+                              ),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () {},
+                              ),
+                            ),
+                          );
                         },
                       )
                     : const Center(
